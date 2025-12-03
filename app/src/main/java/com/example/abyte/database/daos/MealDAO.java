384108bcb9b35b8d3992250839a81e78aefbaebf
Paul <199071975+BoneDude@users.noreply.github.com>
2025-12-03 09:09:22 -0800

    Meals: Added Basic Implementation of Meal DAO

--- app/src/main/java/com/example/abyte/database/daos/MealDAO.java
+++ app/src/main/java/com/example/abyte/database/daos/MealDAO.java
@@ -1,7 +1,31 @@
 package com.example.abyte.database.daos;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import com.example.abyte.database.ByteDatabase;
+import com.example.abyte.database.entities.Meal;
+import com.example.abyte.database.entities.User;
+
+import java.util.List;
 
 @Dao
 public interface MealDAO {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(Meal... meal);
+
+    @Query("Select * from " + ByteDatabase.MEAL_TABLE + " ORDER BY mealName")
+    LiveData<List<Meal>> getAllMeals();
+
+    @Query("DELETE from " + ByteDatabase.MEAL_TABLE)
+    void deleteAll();
+
+    @Query("SELECT * from " + ByteDatabase.MEAL_TABLE + " WHERE mealName == :mealName")
+    LiveData<Meal> getMealByName(String mealName);
+
+    @Query("SELECT * from " + ByteDatabase.MEAL_TABLE + " WHERE mealId == :mealId")
+    LiveData<User> getMealById(int mealId);
 }
