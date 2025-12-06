4f3694eb3080e0e04d722f46e38d8e48658461ae
FlorYDC <lemendez@csumb.edu>
2025-12-05 16:21:07 -0800

    Added white background on the admin xml's Added code in Admin so the adjusted Code in MainActivity could make the admin button work

---
+++ app/src/main/java/com/example/abyte/fragments/Admin.java
@@ -0,0 +1,47 @@
+package com.example.abyte.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.abyte.R;
+
+public class Admin extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState){
+        return inflater.inflate(R.layout.admin_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+        view.findViewById(R.id.viewAdminBugsButton).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //AdminAct_Bugs
+            }
+        });
+        view.findViewById(R.id.viewAdminBugsButton).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //AdminAct_Settings
+            }
+        });
+        view.findViewById(R.id.viewAdminBugsButton).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //AdminAct_Themes
+            }
+        });
+    }
+
+    private void openFragment(Fragment frag){
+        requireActivity().getSupportFragmentManager().beginTransaction().
+                replace(R.id.fragmentContainer, frag).addToBackStack(null).commit();
+    }
+}
