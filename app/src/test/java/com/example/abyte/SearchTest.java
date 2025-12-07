833311ca1f092d2c08473aa1839e1614f0c2a49c
Paul <199071975+BoneDude@users.noreply.github.com>
2025-12-06 19:33:25 -0800

    Meals: Added Tests for categorize iside of SearchTest.java
    Signed-off-by: Paul <199071975+BoneDude@users.noreply.github.com>

--- app/src/test/java/com/example/abyte/SearchTest.java
+++ app/src/test/java/com/example/abyte/SearchTest.java
@@ -9,6 +9,8 @@ import static org.junit.Assert.*;
 import com.example.abyte.database.entities.Ingredient;
 import com.example.abyte.database.entities.Meal;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -20,13 +22,8 @@ import java.util.List;
  */
 public class SearchTest {
     HashMap<String, Ingredient> userIngrMap;
-    Ingredient[] ingredientArr1;
-    Ingredient[] ingredientArr2;
 
     Ingredient[] realIngrArr;
-
-    Meal exampleMeal1;
-    Meal exampleMeal2;
 
     Meal realMeal;
 
@@ -51,13 +48,9 @@ public class SearchTest {
                 new Ingredient("Spaghetti", "oz", 12.0, true)
         };
 
-        exampleMeal1 = new Meal(1, "Meal1", ingredientArr1, "", 0, false);
-        exampleMeal2 = new Meal(2, "Meal2", ingredientArr2, "", 0, false);
         realMeal = new Meal(3, "Spaghetti w/ Tomato Sauce", realIngrArr, "https://www.allrecipes.com/recipe/11715/easy-spaghetti-with-tomato-sauce/", 0, false);
 
         mealList = new ArrayList<>();
-        mealList.add(exampleMeal1);
-        mealList.add(exampleMeal2);
         mealList.add(realMeal);
         search = new Search(mealList, userIngrMap);
     }
@@ -66,12 +59,8 @@ public class SearchTest {
     public void tearDown() {
         userIngrMap = null;
         realIngrArr = null;
-        ingredientArr1 = null;
-        ingredientArr2 = null;
 
         realMeal = null;
-        exampleMeal1 = null;
-        exampleMeal2 = null;
         mealList = null;
 
         search = null;
@@ -81,25 +70,24 @@ public class SearchTest {
     public void constructor_and_getter_test() {
         assertNotNull(search.getMealList());
         assertNotNull(search.getAvailableIngredients());
-        assertNotNull(search.getAvailableMeals());
+        assertNotNull(search.getCategorizedMeals());
         assertNotNull(search.getCheckedMeals());
 
-        assertTrue(search.getAvailableMeals().containsKey(Search.MISSING));
-        assertTrue(search.getAvailableMeals().containsKey(Search.PARTIAL));
-        assertTrue(search.getAvailableMeals().containsKey(Search.FULL));
+        assertTrue(search.getCategorizedMeals().containsKey(Search.MISSING));
+        assertTrue(search.getCategorizedMeals().containsKey(Search.PARTIAL));
+        assertTrue(search.getCategorizedMeals().containsKey(Search.FULL));
 
-        assertNotNull(search.getAvailableMeals().get(Search.MISSING));
-        assertNotNull(search.getAvailableMeals().get(Search.PARTIAL));
-        assertNotNull(search.getAvailableMeals().get(Search.FULL));
+        assertNotNull(search.getCategorizedMeals().get(Search.MISSING));
+        assertNotNull(search.getCategorizedMeals().get(Search.PARTIAL));
+        assertNotNull(search.getCategorizedMeals().get(Search.FULL));
     }
 
     @Test
     public void basic_compare_test() {
         Ingredient testIngredient = new Ingredient("Chicken", "lbs", 1.0, true);
-        ingredientArr1 = new Ingredient[] {testIngredient};
-        exampleMeal1 = new Meal(1, "Meal1", ingredientArr1, "", 0, false);
+        Meal exampleMeal = new Meal(1, "Meal1", new Ingredient[] {testIngredient}, "", 0, false);
         userIngrMap.put("Chicken", new Ingredient("Chicken", "lbs", 1.0, true));
-        HashMap<Integer, List<Ingredient>> testOutput = search.compare(exampleMeal1);
+        HashMap<Integer, List<Ingredient>> testOutput = search.compare(exampleMeal);
 
         assertNotNull(testOutput);
         assertNotNull(testOutput.get(Search.FULL));
@@ -121,7 +109,7 @@ public class SearchTest {
         userIngrList.add(new Ingredient("I", "lbs", 17, false));
         userIngrList.add(new Ingredient("J", "lbs", 19, true));
 
-        ingredientArr1 = new Ingredient[] {
+        Ingredient[] ingredientArr = new Ingredient[] {
                 new Ingredient("A", "lbs", 9, true),
                 new Ingredient("B", "lbs", 10, true),
                 new Ingredient("C", "lbs", 11, true),
@@ -135,30 +123,30 @@ public class SearchTest {
                 new Ingredient("K", "lbs", 19, true),
         };
 
-        exampleMeal1 = new Meal(1, "Lower Than Meal", ingredientArr1, "Missing", 0, false);
+        Meal exampleMeal = new Meal(1, "Lower Than Meal", ingredientArr, "Missing", 0, false);
         for (int i = 0; i < userIngrList.size(); i++) {
             userIngrMap.put(userIngrList.get(i).getName(), userIngrList.get(i));
         }
 
         List<Ingredient> expectedMissingList = new ArrayList<>();
-        expectedMissingList.add(ingredientArr1[0]);
-        expectedMissingList.add(ingredientArr1[1]);
-        expectedMissingList.add(ingredientArr1[10]);
+        expectedMissingList.add(ingredientArr[0]);
+        expectedMissingList.add(ingredientArr[1]);
+        expectedMissingList.add(ingredientArr[10]);
 
         List<Ingredient> expectedPartialList = new ArrayList<>();
-        expectedPartialList.add(ingredientArr1[2]);
-        expectedPartialList.add(ingredientArr1[5]);
-        expectedPartialList.add(ingredientArr1[6]);
-        expectedPartialList.add(ingredientArr1[7]);
+        expectedPartialList.add(ingredientArr[2]);
+        expectedPartialList.add(ingredientArr[5]);
+        expectedPartialList.add(ingredientArr[6]);
+        expectedPartialList.add(ingredientArr[7]);
 
 
         List<Ingredient> expectedFullList = new ArrayList<>();
-        expectedFullList.add(ingredientArr1[3]);
-        expectedFullList.add(ingredientArr1[4]);
-        expectedFullList.add(ingredientArr1[8]);
-        expectedFullList.add(ingredientArr1[9]);
+        expectedFullList.add(ingredientArr[3]);
+        expectedFullList.add(ingredientArr[4]);
+        expectedFullList.add(ingredientArr[8]);
+        expectedFullList.add(ingredientArr[9]);
 
-        HashMap<Integer, List<Ingredient>> actualMap = search.compare(exampleMeal1);
+        HashMap<Integer, List<Ingredient>> actualMap = search.compare(exampleMeal);
 
         System.out.print("Complex Compare: Doing null Test...");
         assertNotNull(actualMap);
@@ -185,5 +173,88 @@ public class SearchTest {
         System.out.print("Complex Compare: Doing Direct Map Check");
         assertEquals(expectedMap, actualMap);
         System.out.println(" Done");
+    }
+
+    @Test
+    public void categorize_meal_error_test() {
+        System.out.print("Categorize Meal Test: Setting up Error Case testing...");
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setErr(new PrintStream(out));
+        System.out.println(" Done");
+
+
+        System.out.print("Categorize Meal Test: Testing Empty checkedMeals Error Case...");
+        assertFalse(search.categorizeMeal(realMeal.getMealName()));
+        assertEquals("ERROR AT CATEGORIZE MEAL: The categorizedMeals Hashmap is empty, problem comparing meal" + System.lineSeparator(), out.toString());
+        System.out.println(" Done");
+        out.reset();
+
+        for (Ingredient i: realMeal.getIngredientArray()) {
+            userIngrMap.put(i.getName(), i);
+        }
+
+        System.out.print("Categorize Meal Test: Method Properly running check...");
+        search.getCheckedMeals().put(realMeal.getMealName(),search.compare(realMeal));
+        System.out.print(" checked meal categorized...");
+        assertTrue(search.categorizeMeal(realMeal.getMealName()));
+        System.out.println(" Done");
+
+        System.out.print("Categorize Meal Test: Doing Direct Map Check...");
+        assertNotNull(search.getCategorizedMeals().get(Search.FULL));
+        assertEquals(realMeal.getMealName(),search.getCategorizedMeals().get(Search.FULL).getFirst());
+        System.out.println(" Done");
+
+        System.out.print("Categorize Meal Test: Testing Wrong Meal Name Error Case...");
+        assertFalse(search.categorizeMeal("Wrong Meal Name"));
+        assertEquals("ERROR AT CATEGORIZE MEAL: The Meal Name \"Wrong Meal Name\" is not in the categorizedMeals Hashmap" + System.lineSeparator(), out.toString());
+        System.out.println(" Done");
+    }
+
+    @Test
+    public void categorize_meal_categorization_test() {
+        System.out.print("Categorize Meal Categorization Test: Setting up...");
+        for (Ingredient i: realMeal.getIngredientArray()) {
+            userIngrMap.put(i.getName(), i);
+        }
+        Ingredient[] ingredientArr1 = new Ingredient[]{
+                new Ingredient("Ground Beef", "lbs", 1.0, true),
+                new Ingredient("Tomato", "cups", 2.5, true),
+                new Ingredient("Pomegranate Molasses", "oz", 39, false),
+        };
+        Ingredient[] ingredientArr2 = new Ingredient[]{
+                new Ingredient("Unobtainium", "lbs", 3, true),
+                new Ingredient("Tomato", "cups", 2.5, true),
+                new Ingredient("Tomato Paste", "oz", 6, false)
+        };
+        Meal exampleMeal1 = new Meal(1, "Meal1", ingredientArr1, "", 0, false);
+        assertNotNull(exampleMeal1);
+        search.getMealList().add(exampleMeal1);
+        Meal exampleMeal2 = new Meal(2, "Meal2", ingredientArr2, "", 0, false);
+        assertNotNull(exampleMeal2);
+        search.getMealList().add(exampleMeal2);
+        System.out.println(" Done");
+
+        System.out.println("Meal List: " + search.getMealList().toString());
+
+        search.getCheckedMeals().put(realMeal.getMealName(),search.compare(realMeal));
+        search.getCheckedMeals().put(exampleMeal1.getMealName(),search.compare(exampleMeal1));
+        search.getCheckedMeals().put(exampleMeal2.getMealName(),search.compare(exampleMeal2));
+
+        assertTrue(search.categorizeMeal(exampleMeal1.getMealName()));
+        assertTrue(search.categorizeMeal(exampleMeal2.getMealName()));
+        assertTrue(search.categorizeMeal(realMeal.getMealName()));
+        assertNotNull(search.getCategorizedMeals().get(Search.FULL));
+        assertNotNull(search.getCategorizedMeals().get(Search.PARTIAL));
+        assertNotNull(search.getCategorizedMeals().get(Search.MISSING));
+        System.out.println(search.getCategorizedMeals().toString());
+
+        System.out.print("Categorize Meal Categorization Test: Checking sizes...");
+        System.out.print(" checking full size...");
+        assertEquals(1, search.getCategorizedMeals().get(Search.FULL).size());
+        System.out.print(" checking partial size...");
+        assertEquals(1, search.getCategorizedMeals().get(Search.PARTIAL).size());
+        System.out.print(" checking missing size...");
+        assertEquals(1, search.getCategorizedMeals().get(Search.MISSING).size());
+
     }
 }
