8edf83d0bd086c1dafae1e94a7b83e4a126851c5
FlorYDC <lemendez@csumb.edu>
2025-12-07 22:42:37 -0800

    Extended both tests

--- app/src/test/java/com/example/abyte/AdminActivityTest.java
+++ app/src/test/java/com/example/abyte/AdminActivityTest.java
@@ -3,34 +3,102 @@ package com.example.abyte;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
-
 import org.junit.Test;
-
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 
-public class AdminActivityTest{
-    //adminActivity is a Fragment subclass
+public class AdminActivityTest {
+    //AdminActivity is a Fragment subclass
     @Test
-    public void adminExtendsFragment(){
+    public void adminExtendsFragment() {
         assertTrue(Fragment.class.isAssignableFrom(AdminActivity.class));
     }
+    //AdminActivity is public and non-abstract
     @Test
-    public void hasOnCreateView() throws NoSuchMethodException{
-        Method onCreateView = AdminActivity.class.getDeclaredMethod("onCreateView",
-                android.view.LayoutInflater.class, android.view.ViewGroup.class,
-                android.os.Bundle.class);
+    public void classIsPublicAndConcrete() {
+        int modifiers = AdminActivity.class.getModifiers();
+        assertTrue("AdminActivity should be public", Modifier.isPublic(modifiers));
+        assertTrue("AdminActivity should not be abstract", !Modifier.isAbstract(modifiers));
+    }
+    @Test
+    public void hasPublicNoArgConstructor() throws NoSuchMethodException {
+        Constructor<AdminActivity> constructor = AdminActivity.class.getConstructor();
+        assertNotNull(constructor);
+        assertTrue("Constructor should be public", Modifier.isPublic(constructor.getModifiers()));
+    }
+
+    @Test
+    public void hasOnCreateView() throws NoSuchMethodException {
+        Method onCreateView = AdminActivity.class.getDeclaredMethod(
+                "onCreateView",
+                LayoutInflater.class,
+                ViewGroup.class,
+                Bundle.class
+        );
         assertNotNull(onCreateView);
-        //Return andorid.view.View
-        assertEquals(android.view.View.class, onCreateView.getReturnType());
+        //Return android.view.View
+        assertEquals(View.class, onCreateView.getReturnType());
+
         //Public
         assertTrue(Modifier.isPublic(onCreateView.getModifiers()));
     }
+    // Check onViewCreated exists and has the expected signature
     @Test
-    public void privateOpenFrag() throws NoSuchMethodException{
+    public void hasOnViewCreated() throws NoSuchMethodException {
+        Method onViewCreated = AdminActivity.class.getDeclaredMethod(
+                "onViewCreated",
+                View.class,
+                Bundle.class
+        );
+        assertNotNull(onViewCreated);
+        // Return type is void
+        assertEquals(void.class, onViewCreated.getReturnType());
+        // Public
+        assertTrue(Modifier.isPublic(onViewCreated.getModifiers()));
+    }
+    // onViewCreated should be an instance method, not static or final
+    @Test
+    public void onViewCreatedIsInstanceMethod() throws NoSuchMethodException {
+        Method onViewCreated = AdminActivity.class.getDeclaredMethod(
+                "onViewCreated",
+                View.class,
+                Bundle.class
+        );
+        int modifiers = onViewCreated.getModifiers();
+        assertTrue("onViewCreated should not be static", !Modifier.isStatic(modifiers));
+        assertTrue("onViewCreated should not be final", !Modifier.isFinal(modifiers));
+    }
+    @Test
+    public void privateOpenFrag() throws NoSuchMethodException {
         Method openFragment = AdminActivity.class.getDeclaredMethod(
-                "openFragment", Fragment.class);
+                "openFragment",
+                Fragment.class
+        );
+        assertNotNull(openFragment);
+        // Return type is void
+        assertEquals(void.class, openFragment.getReturnType());
+        // Should be private
+        assertTrue("openFragment should be private",
+                Modifier.isPrivate(openFragment.getModifiers()));
+        // Should not be static
+        assertTrue("openFragment should not be static",
+                !Modifier.isStatic(openFragment.getModifiers()));
+    }
+    //Ensuring there is only one openFragment method with Fragment parameter
+    @Test
+    public void openFragmentHasSingleFragmentParameter() throws NoSuchMethodException {
+        Method openFragment = AdminActivity.class.getDeclaredMethod(
+                "openFragment",
+                Fragment.class
+        );
+        Class<?>[] params = openFragment.getParameterTypes();
+        assertEquals("openFragment should have exactly one parameter", 1, params.length);
+        assertEquals("Parameter should be a Fragment", Fragment.class, params[0]);
     }
 }
--- app/src/test/java/com/example/abyte/AdminUserControlActivityTest.java
+++ app/src/test/java/com/example/abyte/AdminUserControlActivityTest.java
@@ -2,53 +2,100 @@ package com.example.abyte;
 
 import androidx.fragment.app.Fragment;
 import org.junit.Test;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-public class AdminUserControlActivityTest{
+public class AdminUserControlActivityTest {
+    // AdminUserControlActivity is a Fragment subclass
     @Test
-    public void adminUserControl_extendsFragment(){
+    public void adminUserControl_extendsFragment() {
         assertTrue(Fragment.class.isAssignableFrom(AdminUserControlActivity.class));
     }
+    // Class should be public and non-abstract
     @Test
-    public void adminUserControl_hasOnCreateView() throws NoSuchMethodException{
-        Method onCreateView = AdminUserControlActivity.class.getDeclaredMethod(
-                "onCreateView",
-                android.view.LayoutInflater.class,
-                android.view.ViewGroup.class,
-                android.os.Bundle.class
-        );
-        assertNotNull(onCreateView);
-        //Should return android.view.View
-        assertEquals(android.view.View.class, onCreateView.getReturnType());
-        //Be public
-        assertTrue(Modifier.isPublic(onCreateView.getModifiers()));
+    public void adminUserControl_isPublicAndConcrete() {
+        int modifiers = AdminUserControlActivity.class.getModifiers();
+        assertTrue("AdminUserControlActivity should be public", Modifier.isPublic(modifiers));
+        assertTrue("AdminUserControlActivity should not be abstract", !Modifier.isAbstract(modifiers));
+    }
+    // Fragment must have a public no-arg constructor
+    @Test
+    public void adminUserControl_hasPublicNoArgConstructor() throws NoSuchMethodException {
+        Constructor<AdminUserControlActivity> constructor = AdminUserControlActivity.class.getConstructor();
+        assertNotNull(constructor);
+        assertTrue("Constructor should be public",
+                Modifier.isPublic(constructor.getModifiers()));
     }
     @Test
-    public void adminUserControl_hasOnViewCreated() throws NoSuchMethodException{
-        //Checks if onViewCreated(View v, Bundle b) exists
+    public void adminUserControl_hasOnCreateView() throws NoSuchMethodException {
+        Method onCreateView = AdminUserControlActivity.class.getDeclaredMethod(
+                "onCreateView",
+                LayoutInflater.class,
+                ViewGroup.class,
+                Bundle.class
+        );
+        assertNotNull(onCreateView);
+        // Should return android.view.View
+        assertEquals(View.class, onCreateView.getReturnType());
+        // Be public
+        assertTrue(Modifier.isPublic(onCreateView.getModifiers()));
+    }
+    //onCreateView should be an instance method
+    @Test
+    public void adminUserControl_onCreateViewIsInstanceMethod() throws NoSuchMethodException {
+        Method onCreateView = AdminUserControlActivity.class.getDeclaredMethod(
+                "onCreateView", LayoutInflater.class, ViewGroup.class, Bundle.class
+        );
+        int modifiers = onCreateView.getModifiers();
+        assertTrue("onCreateView should not be static", !Modifier.isStatic(modifiers));
+        assertTrue("onCreateView should not be final", !Modifier.isFinal(modifiers));
+    }
+    @Test
+    public void adminUserControl_hasOnViewCreated() throws NoSuchMethodException {
+        // Checks if onViewCreated(View v, Bundle b) exists
         Method onViewCreated = AdminUserControlActivity.class.getDeclaredMethod(
-                "onViewCreated", android.view.View.class, android.os.Bundle.class
+                "onViewCreated",
+                View.class,
+                Bundle.class
         );
         assertNotNull(onViewCreated);
-        //Return void
+        // Return void
         assertEquals(void.class, onViewCreated.getReturnType());
-        //Should be public
+        // Should be public
         assertTrue(Modifier.isPublic(onViewCreated.getModifiers()));
     }
     @Test
-    public void adminUserControl_hasPrivateOpenFragment() throws NoSuchMethodException{
-        //If openFragment(Fragment) such as AdminAct_Bugs exists
+    public void adminUserControl_hasPrivateOpenFragment() throws NoSuchMethodException {
+        //If openFragment(Fragment) exists
         Method openFragment = AdminUserControlActivity.class.getDeclaredMethod(
                 "openFragment", Fragment.class
         );
         assertNotNull(openFragment);
-        //Retrun void
+        //Return void
         assertEquals(void.class, openFragment.getReturnType());
         //Private
-        assertTrue(Modifier.isPrivate(openFragment.getModifiers()));
+        assertTrue("openFragment should be private",
+                Modifier.isPrivate(openFragment.getModifiers()));
+        //Not static
+        assertTrue("openFragment should not be static",
+                !Modifier.isStatic(openFragment.getModifiers()));
+    }
+    // Ensure openFragment has exactly one Fragment parameter
+    @Test
+    public void adminUserControl_openFragmentHasSingleFragmentParameter() throws NoSuchMethodException {
+        Method openFragment = AdminUserControlActivity.class.getDeclaredMethod(
+                "openFragment", Fragment.class
+        );
+        Class<?>[] params = openFragment.getParameterTypes();
+        assertEquals("openFragment should have exactly one parameter", 1, params.length);
+        assertEquals("Parameter should be a Fragment", Fragment.class, params[0]);
     }
 }
